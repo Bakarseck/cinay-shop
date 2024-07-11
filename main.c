5816b9ef9817341dacf553f072866272b4762404
bakseck <seck.bakar@ugb.edu.sn>
2024-07-11 12:40:01 +0000

    add: ScannerInt

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+// #include "headers/user.h"
+// #include "headers/category.h"
+// #include "headers/product.h"
+// #include "headers/sales.h"
+// #include "headers/utils.h"
+
+int main()
+{
+     int choix;
+     // initialiser();
+
+     while (1)
+     {
+          printf("\n--- Menu Principal ---\n");
+          printf("1. Connexion\n");
+          printf("2. Quitter\n");
+          printf("Choisissez une option: ");
+          scanf("%d", &choix);
+
+          switch (choix)
+          {
+          case 1:
+               // connexion();
+               break;
+          case 2:
+               printf("Au revoir !\n");
+               return 0;
+          default:
+               printf("Option invalide !\n");
+          }
+     }
+
+     return 0;
+}
---
+++ utils.c
@@ -0,0 +1,11 @@
+#include "utils.h"
+#include <stdio.h>
+
+void initialiser() {
+    printf("Initialisation du système\n");
+}
+
+// Permet de récupérer un int à partir de son adresse
+void ScannerInt(int *a) {
+
+}
